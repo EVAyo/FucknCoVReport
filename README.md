1a2911a9a72d356b42c9a7d1e783120f83326b49
hanrc97 <hanrc1997@gmail.com>
2021-01-24 17:34:06 +0800

    Updated

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # FucknCoVReport
-An automatic python script for CUGB's school-leaving and COVID-19 report  
-中国地质大学（北京）疫情期间备案制出校自动申请与每日自动进行健康打卡上报  
-***免责声明（Disclaimer）：使用该脚本而可能引起的法律责任和学校追究等责任由使用者个人承担，与开发者无关，请勿滥用。为了您和他人的健康着想，请如实填写出校信息，在有位置变动和情况变化时手动填写申报！***
+An automatic python script for CUGB's COVID-19 report  
+中国地质大学（北京）疫情期间每日自动进行健康打卡上报  
+***免责声明（Disclaimer）：使用该脚本而可能引起的法律责任和学校追究等责任由使用者个人承担，与开发者无关，请勿滥用。为了您和他人的健康着想，请如实填写信息，在有位置变动和情况变化时手动填写申报！***
 ## UPDATES
-### **2020/10/03 - This update version resolved:**
-- Add new capabilities related to school-leaving
-### **2020/08/25 (Upcoming) - The upcoming update version will resolve:**  
+### **2021/01/24 - This update version resolved:**
+- Be adapted to the new CAS certification and middle/high risk regions
+### **~~2020/10/03 - This update version resolved:~~**
+- ~~Add new capabilities related to school-leaving~~
+### **2020/08/25 - The upcoming update version will resolve:**  
 - In the upcoming campus life, the Big Brother will take the MAE (Morning, Afteroon and Evening) clock-in measures.
 - Epidemic prevention should not torment students and the other ordinary people and formalism does harm.  
 ### **2020/08/14 - This update version resolved:**  
@@ -17,21 +19,14 @@ pip install requests
 ### Modify
 >main.py:  
 >>  
->>line 33:  ```uname = 'example'  # Your Student Number, e.g. 2001200001```  
->>line 34:  ```upwd = 'example'  # You ID card: last 6 numbers, e.g. 123456```  
+>>line 16:  ```self.uname = ''  # Your Student Number, e.g. 2001200001```  
+>>line 17:  ```self.upwd = ''  # You ID card: last 6 numbers, e.g. 123456```  
 >>**For example:  
 >>'200120000X' (Your Student Number), '123456' (The Last Six Number of Your ID Card)**  
 >>  
->>line 64:  ``` ...,"location_address":"浙江省XX市XX街道XX社区","location_longitude":"123.123123","location_latitude":"32.32132",..."```  
+>>line 81:  ```"location_longitude":"123.123123", "location_latitude":"45.45454","location_address":"XX省XX市XX街道XX小区"}'''```  
 >>**Notice:  
 >>The value of "location_longitude" should be like "123.123123", and the value of "location_latitude" should be like "32.32132".In addtion, the two values should be matched with the value of "location_address".**
->>  
->iMessage.py:
->>line 9:  ```"from": "example@example.com",``` **e.g. username(send_from)@163.com**  
->>line 10:  ```"to": "example@example.com",``` **e.g. username(send_to)@gmail.com**  
->>line 11:  ```"hostname": "smtp.example.com",``` **e.g. smtp.163.com**  
->>line 12:  ```"username": "example@example.com",``` **e.g. username(send_from)@163.com**  
->>line 13:  ```"password": "******",``` **e.g. 163mail API key**
 >>  
 ### Run
 WINDOWS:  ```python main.py```
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 # coding:utf-8
 import requests
 from http import cookiejar
+from bs4 import BeautifulSoup
 import time
-import iMessage
+# import iMessage
 import base64
+import datetime
+import random
+import esprima
+import re
 
 
 class STUPost:
     def __init__(self):
-        self.uname = 'example'  # Your Student Number, e.g. 2001200001
-        self.upwd = 'example'  # You ID card: last 6 numbers, e.g. 123456
+        self.uname = ''  # Your Student Number, e.g. 2001200001
+        self.upwd = ''  # You ID card: last 6 numbers, e.g. 123456
         self.cookies = {}
         # mock the build-in browser of WeChat:
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Linux; Android 10; PCT-AL10 Build/HUAWEIPCT-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2575 MMWEBSDK/200701 Mobile Safari/537.36 MMWEBID/4039 MicroMessenger/7.0.17.1720(0x27001137) Process/tools WeChat/arm64 NetType/WIFI Language/en ABI/arm64',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 10; PCT-AL10 Build/HUAWEIPCT-AL10; wv) AppleWebKit/537.36 ('
+                          'KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2575 MMWEBSDK/200701 Mobile '
+                          'Safari/537.36 MMWEBID/4039 MicroMessenger/7.0.17.1720(0x27001137) Process/tools '
+                          'WeChat/arm64 NetType/WIFI Language/en ABI/arm64',
             'Referer': 'http://stu.cugb.edu.cn/webApp/xuegong/index.html',
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
         # mock the others
@@ -21,40 +29,46 @@ class STUPost:
         self.session = requests.session()
         self.message1 = ''
         self.message2 = ''
+        self.subj = ''
+        self.content = ''
 
     def login(self):
         try:
-            # Request the cookie and token from the origin web:
-            token_url = 'http://stu.cugb.edu.cn'
-            req = self.session.get(token_url)
-            set_cookie = requests.utils.dict_from_cookiejar(req.cookies)
-            header_cookie = 'JSESSIONID' + '=' + str(set_cookie['JSESSIONID']) + ';' \
-                            + 'token' + '=' + str(set_cookie['token'])
-
-            # Add the cookie and token into headers:
-            self.headers['Cookie'] = header_cookie
-            url = 'http://stu.cugb.edu.cn//login/Login.htm'
-            # A new feature for the site that uses base64 encoding for the username and password, and uses token:
-            uname_encrypt = str(base64.b64encode(self.uname.encode('utf-8')), 'utf-8')
-            upwd_encrypt = str(base64.b64encode(self.upwd.encode('utf-8')), 'utf-8')
-            token = str(set_cookie['token'])
-            data = {'username': uname_encrypt, 'password': upwd_encrypt, 'verification': '', 'token': token}
-            self.session.post(url=url, data=data, headers=self.headers)
-            time.sleep(10)
-            # cookies = requests.utils.dict_from_cookiejar(req.cookies)
-            # for key in cookies:
-            #     cookie = ""
-            #     cookie = cookie + key + "=" + str(cookies[key])
-            # cookie = cookie + "; username=example; menuVisible=0" # For example: 200119000X (Your Student Number)
-            # The link used in the bulit-in browser of WeChat：
+            url = 'https://cas.cugb.edu.cn/login'
+            req = self.session.request('GET', url, verify=False).content
+            soup = BeautifulSoup(req, 'html.parser')
+            execution = soup.findAll("input", {"name": "execution"})[0]["value"]
+            system = soup.findAll("input", {"id": "userLoginSystem"})[0]["value"]
+            data = {'username': self.uname,
+                    'password': self.upwd,
+                    'execution': execution,
+                    '_eventId': 'submit',
+                    'geolocation': '',
+                    'loginType': 'username',
+                    'system': system,  # '27A5A4DF0C874122A0AFE0367F0A3F46'
+                    'enableCaptcha': 'N'}
+            req = self.session.post(url=url, data=data, headers=self.headers, verify=False)
+            self.cookies = requests.utils.dict_from_cookiejar(req.cookies)
+            time.sleep(3)
+            # To get the uid from javascript:
+            uidurl = 'https://stu.cugb.edu.cn/'
+            req = self.session.request('GET', uidurl, cookies=self.cookies, headers=self.headers, verify=False).content
+            soup = BeautifulSoup(req, 'html.parser')
+            scriptTags = str(soup.findAll('script')[1])
+            rexp = re.compile(r'<[^>]+>', re.S)
+            scriptCode = rexp.sub('', scriptTags)
+            uid = esprima.tokenize(scriptCode)[48].value.replace('\'', '')
+            uiddata = {'uid': uid}
+            req = self.session.request('POST', "https://stu.cugb.edu.cn:443/caswisedu/login.htm", data=uiddata,
+                                       verify=False)
+            time.sleep(3)
             content = self.session.post(
-                'http://stu.cugb.edu.cn/webApp/xuegong/index.html#/zizhu/apply?projectId=4a4ce9d6725c1d4001725e38fbdb07cd&type=YQSJCJ')
-            # The Link used in the web browsers (random stamp/timestamp exists, unresolved):
-            # content = self.session.post(
-            #   'http://stu.cugb.edu.cn:80/syt/zzapply/apply.htm?type=yqsjcj&judge=sq&xmid=4a4ce9d6725c1d4001725e38fbdb07cd&_t=809439&_winid=w6236')
+                'http://stu.cugb.edu.cn/webApp/xuegong/index.html#/zizhu/apply?projectId'
+                '=4a4ce9d6725c1d4001725e38fbdb07cd&type=YQSJCJ',
+                verify=False)
             if content.status_code == 200:
                 self.message1 = "Login status: Succeeded"
-                time.sleep(10)
+                time.sleep(3)
                 self.clock_in()
             else:
                 self.message1 = "Login status: Failed"
@@ -64,27 +78,64 @@ class STUPost:
     def clock_in(self):
         # cookie_para = {i.split("=")[0]: i.split("=")[1] for i in cookie.split("; ")}
         data = {
-            'data': '''{"xmqkb":{"id":"4a4ce9d6725c1d4001725e38fbdb07cd"},"location_address":"北京市海淀区学院路街道成府路18号中国地质大学(北京)","location_longitude":"116.349317","location_latitude":"39.99266","c1":"36.9℃以下","c2":"健康","c17":"否","c4":"否","c5":"否","c6":"否","c18":"正常","c7":"否","type":"YQSJCJ"}''',
-            #'data': '''{"xmqkb":{"id":"4a4ce9d6725c1d4001725e38fbdb07cd"},"location_address":"浙江省XX市XX街道XX社区","location_longitude":"123.123123","location_latitude":"32.32132","c1":"36.9℃以下","c2":"健康","c17":"否","c4":"否","c5":"否","c6":"否","c18":"正常","c7":"否","type":"YQSJCJ"}''',
+            'data': '''{"xmqkb":{"id":"4a4ce9d6725c1d4001725e38fbdb07cd"},"c1":"36.9℃以下","c2":"健康","c17":"否",
+            "c4":"否","c5":"否","c6":"否","c9":"否","c7":"否","type":"YQSJCJ",
+            "location_longitude":"123.123123", "location_latitude":"45.45454","location_address":"XX省XX市XX街道XX小区"}''',
+            # 'data': '''{"xmqkb":{"id":"4a4ce9d6725c1d4001725e38fbdb07cd"},"location_address":"浙江省XX市XX街道XX社区",
+            # "location_longitude":"123.123123","location_latitude":"32.32132","c1":"36.9℃以下","c2":"健康","c17":"否",
+            # "c4":"否","c5":"否","c6":"否","c18":"正常","c7":"否","type":"YQSJCJ"}''',
             'msgUrl': '''syt/zzapply/list.htm?type=YQSJCJ&xmid=4a4ce9d6725c1d4001725e38fbdb07cd''',
             'uploadFileStr': '''{}''', 'multiSelectData': '''{}'''}
         # Notice: "location_longitude" should be like "123.123123", and "location_latitude" should be like "32.32132"
-        # data = '''{{'xmqkb':{"id":"4a4ce9d6725c1d4001725e38fbdb07cd"},"location_address":"","location_longitude":"","location_latitude":"","c1":"36.9℃以下","c2":"健康","c17":"否","c4":"否","c5":"否","c6":"否","c18":"正常","c7":"否","type":"YQSJCJ"},'msgUrl': "syt/zzapply/list.htm?type=YQSJCJ&xmid=4a4ce9d6725c1d4001725e38fbdb07cd",'uploadFileStr': "{}",'multiSelectData': "{}"}}'''
-        try:
-            r = self.session.request('POST', url='http://stu.cugb.edu.cn/syt/zzapply/operation.htm',
-                                     headers=self.headers, data=data)
-            # print(r.status_code)
-            if r.text == 'success':
-                self.message2 = 'Clocking-in status: Succeeded'
-            elif r.text == 'Applied today':
-                self.message2 = 'Clocking-in status: Applied today'
+        # data = '''{{'xmqkb':{"id":"4a4ce9d6725c1d4001725e38fbdb07cd"},"location_address":"",
+        # "location_longitude":"","location_latitude":"","c1":"36.9℃以下","c2":"健康","c17":"否","c4":"否","c5":"否",
+        # "c6":"否","c18":"正常","c7":"否","type":"YQSJCJ"},'msgUrl':
+        # "syt/zzapply/list.htm?type=YQSJCJ&xmid=4a4ce9d6725c1d4001725e38fbdb07cd",'uploadFileStr': "{}",
+        # 'multiSelectData': "{}"}}'''
+        while True:
+            try:
+                r = self.session.request('POST', url='https://stu.cugb.edu.cn:443/syt/zzapply/operation.htm',
+                                         headers=self.headers, cookies=self.cookies, data=data, verify=False)
+                # print(r.status_code)
+                if r.text == 'success':
+                    self.message2 = 'Clocking-in status: Succeeded'
+                    self.subj = '☛已提交每日健康信息...'
+                    self.content = """
+                    ✔今日打卡成功！
+                    """
+                elif r.text == 'Applied today':
+                    self.message2 = 'Clocking-in status: Applied today'
+                    self.subj = '☛健康信息今日已提交！'
+                    self.content = """
+                    ⚠已经打卡啦！
+                    """
+                else:
+                    self.message2 = 'Clocking-in status: Failed. Please check it'
+                    self.subj = '☛出现异常，请查看日志！'
+                    self.content = """
+                    ✘出错啦，请检查！
+                    """
+            except Exception as e:
+                self.message2 = 'Error Code 1: ' + str(e)
+                self.subj = '☛出现异常，请查看日志！'
+                self.content = """
+                【⚠警告！抛出异常代码！⚠】
+                %s
+                """ % self.message2
             else:
-                self.message2 = 'Clocking-in status: Failed. Please check it'
-        except Exception as e:
-            self.message2 = 'Error Code 1: ' + str(e)
+                break
+
+    def send_to_wechat(self):
+        api = "https://sc.ftqq.com/[Your API code].send"
+        data = {
+            "text": self.subj,
+            "desp": self.content}
+        requests.post(api, data=data, verify=False)
+
 
 if __name__ == '__main__':
     student = STUPost()
     student.login()
-    iMessage.send_Message(News=student.message1 + "\n" + student.message2,
-                          sub="FucknCoVReport: " + student.message2)
+    # iMessage.send_Message(News=student.message1 + "\n" + student.message2,
+    #                      sub="FucknCoVReport: " + student.message2)
+    student.send_to_wechat()
