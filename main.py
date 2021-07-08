51ca4fc809d29f57a8235d7b8edd2fe1d0f53f1c
hanrc97 <hanrc1997@gmail.com>
2021-07-08 21:33:27 +0800

    Some new features

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import requests
 from http import cookiejar
 from bs4 import BeautifulSoup
 import time
-# import iMessage
+#import iMessage
 import base64
 import datetime
 import random
 import esprima
 import re
 
-
 class STUPost:
     def __init__(self):
         self.uname = ''  # Your Student Number, e.g. 2001200001
-        self.upwd = ''  # You ID card: last 6 numbers, e.g. 123456
+        self.upwd = ''  # Last 6 numbers of your ID card, e.g. 123456 (or the password which is set by yourself)
         self.cookies = {}
         # mock the build-in browser of WeChat:
         self.headers = {
@@ -29,8 +28,6 @@ class STUPost:
         self.session = requests.session()
         self.message1 = ''
         self.message2 = ''
-        self.subj = ''
-        self.content = ''
 
     def login(self):
         try:
@@ -63,8 +60,7 @@ class STUPost:
                                        verify=False)
             time.sleep(3)
             content = self.session.post(
-                'http://stu.cugb.edu.cn/webApp/xuegong/index.html#/zizhu/apply?projectId'
-                '=4a4ce9d6725c1d4001725e38fbdb07cd&type=YQSJCJ',
+                'http://stu.cugb.edu.cn/webApp/xuegong/index.html#/zizhu/apply?projectId=4a4ce9d6725c1d4001725e38fbdb07cd&type=YQSJCJ',
                 verify=False)
             if content.status_code == 200:
                 self.message1 = "Login status: Succeeded"
@@ -75,27 +71,22 @@ class STUPost:
         except Exception as e:
             self.message1 = 'Error Code 0: ' + str(e)
 
+
+
     def clock_in(self):
         # cookie_para = {i.split("=")[0]: i.split("=")[1] for i in cookie.split("; ")}
         data = {
-            'data': '''{"xmqkb":{"id":"4a4ce9d6725c1d4001725e38fbdb07cd"},"c1":"36.9℃以下","c2":"健康","c17":"否",
-            "c4":"否","c5":"否","c6":"否","c9":"否","c7":"否","type":"YQSJCJ",
+            'data': '''{"xmqkb":{"id":"4a4ce9d6725c1d4001725e38fbdb07cd"},"c1":"37.2℃及以下","c2":"健康","c17":"否",
+            "c4":"否","c5":"否","c6":"否","c9":"否","c7":"否","c19":"36.5","c22":"36.5","type":"YQSJCJ",
             "location_longitude":"123.123123", "location_latitude":"45.45454","location_address":"XX省XX市XX街道XX小区"}''',
-            # 'data': '''{"xmqkb":{"id":"4a4ce9d6725c1d4001725e38fbdb07cd"},"location_address":"浙江省XX市XX街道XX社区",
-            # "location_longitude":"123.123123","location_latitude":"32.32132","c1":"36.9℃以下","c2":"健康","c17":"否",
-            # "c4":"否","c5":"否","c6":"否","c18":"正常","c7":"否","type":"YQSJCJ"}''',
+            # Note: Please change the Line 81
             'msgUrl': '''syt/zzapply/list.htm?type=YQSJCJ&xmid=4a4ce9d6725c1d4001725e38fbdb07cd''',
             'uploadFileStr': '''{}''', 'multiSelectData': '''{}'''}
         # Notice: "location_longitude" should be like "123.123123", and "location_latitude" should be like "32.32132"
-        # data = '''{{'xmqkb':{"id":"4a4ce9d6725c1d4001725e38fbdb07cd"},"location_address":"",
-        # "location_longitude":"","location_latitude":"","c1":"36.9℃以下","c2":"健康","c17":"否","c4":"否","c5":"否",
-        # "c6":"否","c18":"正常","c7":"否","type":"YQSJCJ"},'msgUrl':
-        # "syt/zzapply/list.htm?type=YQSJCJ&xmid=4a4ce9d6725c1d4001725e38fbdb07cd",'uploadFileStr': "{}",
-        # 'multiSelectData': "{}"}}'''
         while True:
             try:
                 r = self.session.request('POST', url='https://stu.cugb.edu.cn:443/syt/zzapply/operation.htm',
-                                         headers=self.headers, cookies=self.cookies, data=data, verify=False)
+                                        headers=self.headers, cookies=self.cookies, data=data, verify=False)
                 # print(r.status_code)
                 if r.text == 'success':
                     self.message2 = 'Clocking-in status: Succeeded'
@@ -112,9 +103,7 @@ class STUPost:
                 else:
                     self.message2 = 'Clocking-in status: Failed. Please check it'
                     self.subj = '☛出现异常，请查看日志！'
-                    self.content = """
-                    ✘出错啦，请检查！
-                    """
+                    self.content = self.message2
             except Exception as e:
                 self.message2 = 'Error Code 1: ' + str(e)
                 self.subj = '☛出现异常，请查看日志！'
@@ -132,10 +121,9 @@ class STUPost:
             "desp": self.content}
         requests.post(api, data=data, verify=False)
 
-
 if __name__ == '__main__':
     student = STUPost()
     student.login()
-    # iMessage.send_Message(News=student.message1 + "\n" + student.message2,
+    #iMessage.send_Message(News=student.message1 + "\n" + student.message2,
     #                      sub="FucknCoVReport: " + student.message2)
     student.send_to_wechat()
